05886b0fea0b4d277885b558d619c971017d5820
Eston Naicker <naickereston@gmail.com>
2023-11-16 20:35:55 +0200

    finish part 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,19 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import { notes } from "../notes";
+
+function createNote({key, title, content}) {
+    return <Note key={key} title={title} content={content}/>
+}
 
 export default function App() {
     return (
         <div>
             <Header></Header>
-            <Note></Note>
+            {
+                notes.map(createNote)
+            }
             <Footer></Footer>
         </div>
     );
